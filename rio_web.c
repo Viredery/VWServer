14f29fbc168c1ff21544a9022c9a7c313a7c1c36
Viredery <dyrviredery@hotmail.com>
2016-04-11 19:06:21 +0800

    robust I/O with buffer

--- rio_web.c
+++ rio_web.c
@@ -1,27 +1,7 @@
-/*rio包自动处理Unix read函数和Unix write函数的不足值情况
-  这里使用的带缓冲rio输入函数是线程安全的
-  使用rio_readlineb和rio_readnb前应先调用rio_readinitb函数*/
-#include <unistd.h>
-#include <memory.h>
-#include <errno.h>
-#define RIO_BUFSIZE 8192
-
-typedef struct {
-	int rio_fd;
-	int rio_cnt;
-	char *rio_bufptr;
-	char rio_buf[RIO_BUFSIZE];
-} rio_t;
-
-ssize_t rio_read(rio_t *rp, void *usrbuf, size_t n);
-
-ssize_t rio_readinitb(rio_t *rp, int fd);
-ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
-ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
-
-ssize_t rio_writen(int fd, void *usrbuf, size_t n);
-
-/*从位置usrbuf传送n个字节到描述符fd。绝不会返回不足值*/
+#include "rio_web.h"
+/*
+ * 从位置usrbuf传送n个字节到描述符fd。绝不会返回不足值
+ */
 ssize_t rio_writen(int fd, void *usrbuf, size_t n)
 {
 	size_t nleft = n;
@@ -42,14 +22,18 @@ ssize_t rio_writen(int fd, void *usrbuf, size_t n)
 	}
 	return n;
 }
-
-ssize_t rio_readinitb(rio_t *rp, int fd)
+/*
+ * rio_readinitb函数将rio与文件描述符结合起来，完成带缓冲读的初始化
+ */
+void rio_readinitb(rio_t *rp, int fd)
 {
 	rp->rio_fd = fd;
 	rp->rio_cnt = 0;
 	rp->rio_bufptr = rp->rio_buf;
 }
-/*rio_read函数是Unix read函数的带缓冲版本*/
+/*
+ * rio_read函数是Unix read函数的带缓冲版本
+ */
 ssize_t rio_read(rio_t *rp, void *usrbuf, size_t n)
 {
 	int cnt;
@@ -74,7 +58,10 @@ ssize_t rio_read(rio_t *rp, void *usrbuf, size_t n)
 	rp->rio_cnt -= cnt;
 	return cnt;
 }
-/*最多读取maxlen-1个字节，余下的一个字符留给结尾的空字符。超过maxlen-1字节的文本行被截断，并用一个空字符结束*/
+/*
+ * 最多读取maxlen-1个字节，余下的一个字符留给结尾的空字符。
+ * 超过maxlen-1字节的文本行被截断，并用一个空字符结束
+ */
 ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
 {
 	int n, rc;
@@ -98,13 +85,15 @@ ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
 	*bufp = 0;
 	return n;
 }
-/*从rp最多读n个字节到存储器位置usrbuf*/
+/*
+ * 从rp最多读n个字节到存储器位置usrbuf
+ */
 ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
 {
 	ssize_t nleft = n;
 	ssize_t nread;
 	char *bufp = usrbuf;
-	while(nleft > 0)
+	while(nleft > 0) 
 	{
 		if((nread = rio_read(rp, bufp, nleft)) < 0)
 		{
